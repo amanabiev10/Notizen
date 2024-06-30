6ac0723eb433918435eee0e3722d96435fdd1ee8
Aman Abiev <amanabiev10@gmail.com>
2024-06-30 20:58:44 +0200

    Frontend Update 30.06.2024

--- notes/urls.py
+++ notes/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = 'notes'
 
 urlpatterns = [
+    path('/<int:note_id>/', views.notes_views, name='notes_views'),
     path('page/<int:page_id>/', views.page_detail, name='page_detail'),
     path('add_notebook/', views.add_notebook, name='add_notebook'),
     path('add_page/', views.add_page, name='add_page'),
